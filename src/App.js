4346978d9f2d7facdf7c1d855ba740c8d4a49948
BlendMuhaxheri <blend11muhaxheri@gmail.com>
2021-01-20 17:14:10 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import AboutUs from './pages/AboutUs'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>capture</h1>
+      <AboutUs/>
+    
+    </div>
+  );
+}
+
+export default App;
